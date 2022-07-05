d5e4cd00eb0471486ab16796182c68e39bd66327
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-04 23:21:01 -0300

    primeiro commit

---
+++ CONSUMO_MEDIO_CARRO/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace CONSUMO_MEDIO_CARRO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Distância (Km): ");
+            int km = int.Parse(Console.ReadLine());
+            Console.WriteLine("Litros consumidos (L): ");
+            double litros = double.Parse(Console.ReadLine());
+
+            double consMedio = km / litros;
+            Console.WriteLine("Consumo médio : {0}km/l", consMedio.ToString("F3", CultureInfo. InvariantCulture));
+
+
+        }
+    }
+}
